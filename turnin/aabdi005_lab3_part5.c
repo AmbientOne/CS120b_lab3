a7f2f7f1eff8c8e588234c63cea776d222ddd39b
Abdirahman Abdi <aabdi005@wch136-24.cs.ucr.edu>
2021-10-10 13:04:14 -0700

    Stuff

---
+++ turnin/aabdi005_lab3_part5.c
@@ -0,0 +1,34 @@
+/*	Author: aabdi005
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF; PORTB = 0x00;
+    DDRD= 0xFE; PORTD = 0x00;
+
+    unsigned char tmpA = 0x00;
+    unsigned char tmpB = 0x00;
+    unsigned char tmpD = 0x00;
+    unsigned short totalWeight = 0;
+
+    /* Insert your solution below */
+    while(1){
+        tmpB = PINB;
+        tmpD = PIND;
+        
+        
+}
+    return 1;
+}
